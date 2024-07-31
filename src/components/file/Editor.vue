03cf58e2c531b031e0ffa32749d734ada1d71b19
8AH <91540667+8AH@users.noreply.github.com>
2024-07-31 16:35:25 +0200

    try to add "" into content

--- src/components/file/Editor.vue
+++ src/components/file/Editor.vue
@@ -406,6 +406,9 @@ const save = async () => {
     content = model.value;
   }
   
+  // This is not a test, this is rock n roll
+  content = '"'content'"'
+  
   try {
     // If it's a new file, we need to generate a filename
     if (!sha.value) {
